a9719765670ad3bcb5f8be4778c751d07f022348
现充 <qixiang.cqx@alibaba-inc.com>
2017-03-05 20:55:06 +0800

    use ejs instead of pug

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 
 const Koa = require('koa')
+const ejs = require('koa-ejs')
 const route = require('koa-route')
 const cheerio = require('cheerio')
 const got = require('got')
-const pug = require('pug')
 
 const app = module.exports = new Koa()
+ejs(app, { root: __dirname, layout: false, viewExt: 'ejs' })
 app.use(route.get('/', async ctx => ctx.redirect('/~gerhut')))
 app.use(route.get('/~:user', async (ctx, user) => {
   const response = await got(`https://www.npmjs.com/~${user}`)
   const $ = cheerio.load(response.body)
-  const pkgs = $('.collaborated-packages a')
-    .map((i, el) => $(el).text()).get()
-  ctx.body = pug.renderFile('user.pug', { user, pkgs })
+  const pkgs = $('.collaborated-packages a').map((i, el) => $(el).text()).get()
+  return await ctx.render('user', { user, pkgs })
 }))
 
 if (require.main === module) {
