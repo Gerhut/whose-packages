4447872ce091ab912aa4daa2a486151772490aec
现充 <qixiang.cqx@alibaba-inc.com>
2017-03-06 13:16:02 +0800

    remove koa-route

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ require('dotenv').config()
 
 const Koa = require('koa')
 const ejs = require('koa-ejs')
-const route = require('koa-route')
 const cheerio = require('cheerio')
 const got = require('got')
 
 const app = module.exports = new Koa()
 ejs(app, { root: __dirname, layout: false, viewExt: 'ejs' })
-app.use(route.get('/', async ctx => ctx.redirect('/~gerhut')))
-app.use(route.get('/~:user', async (ctx, user) => {
+app.use(async ctx => {
+  const match = ctx.path.match(/^\/(\w*)$/)
+  ctx.assert(match, 404)
+
+  const user = match[1]
+  if (user === '') return ctx.redirect(`${ctx.origin}/gerhut`)
+
   const response = await got(`https://www.npmjs.com/~${user}`)
   const $ = cheerio.load(response.body)
   const pkgs = $('.collaborated-packages a').map((i, el) => $(el).text()).get()
-  return await ctx.render('user', { user, pkgs })
-}))
+
+  return ctx.render('user', { user, pkgs })
+})
 
 if (require.main === module) {
   app.listen(process.env.PORT)
